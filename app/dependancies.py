cc029229d41b258036d57bed0c40b2d11133a320
yamen <62008653+weaponized-fetus@users.noreply.github.com>
2023-02-21 19:47:13 +0200

    minor changes

--- app/dependancies.py
+++ app/dependancies.py
@@ -1,14 +1,33 @@
+""""Shared module"""
 import random
 from typing import List
 
 class Classifier():
+    """Dummy class
+
+    Returns:
+        Classifier: class that contains the dummy model
+    """
     classes = ['cs.ML', 'cs.AI', 'cs.LG']
     def predict(self, X: List[str]) -> List[str]:
+        """Return dummy 
+
+        Args:
+            X (List[str]): List of abstracts to classify
+
+        Returns:
+            List[str]: List of categories as strings, same length as input array
+        """
         return [self.classes[random.randrange(0, 3)] for _ in X]
 
 __model = Classifier()
 
 def get_model():
+    """Returns an instance of a classifier model
+
+    Returns:
+        Classifier:  the model
+    """
     if __model:
         return __model
     return Classifier()
