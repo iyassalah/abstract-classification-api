2b16cd54d11d70e78c7768aac9e3a40cb40d678b
yamen <62008653+weaponized-fetus@users.noreply.github.com>
2023-02-25 19:05:52 +0200

    #1: read config from env file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 """Entry point for the app, run this using uvicorn"""
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from .routers import batch, interactive
+
+load_dotenv('../.env.dev')
 
 app = FastAPI()
 
@@ -11,6 +14,5 @@ app.include_router(interactive.router)
 
 @app.get("/")
 async def root():
-    """TODO: add things here
-    """
+    """TODO: add things here"""
     return {"message": "hi"}
