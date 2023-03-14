4567557111d6428d925ddadfd55869285e046b7b
iyassalah <iyas.a.salah@transcend.ps>
2023-03-14 10:39:06 +0200

    Create needed class for abstact and hyperparamerter

--- app/models.py
+++ app/models.py
@@ -2,6 +2,24 @@
 from datetime import datetime
 from pydantic import BaseModel
 
+class Abstract(BaseModel):
+    """Abstact class"""
+    name: str
+    category: list[str]
+    retrieved: datetime
+    source: str
+    doi: str
+    prediction: list[str]
+
+class Hyperparameter(BaseModel):
+    """Hyperparameter class"""
+    created: datetime
+    params: object
+    fp: int
+    tp: int
+    fn: int
+    tn: int
+
 class BatchModel(BaseModel):
     """Data model for list of abstracts payload used in batch processing
     
