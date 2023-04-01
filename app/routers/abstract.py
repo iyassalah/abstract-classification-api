7533c9f1c5a832e635aa17dec4207e54a9d528b9
iyassalah <iyas.a.salah@transcend.ps>
2023-04-01 23:45:22 +0300

    Add abstact and hyperparameter router.

---
+++ app/routers/abstract.py
@@ -0,0 +1,37 @@
+"""Router for abstact"""
+
+from bson import ObjectId
+from fastapi import APIRouter, HTTPException
+from ..database import db
+from ..models import Abstract
+
+router = APIRouter(prefix="/Abstract")
+
+@router.post("/abstact")
+async def create_abstact(abstact: Abstract):
+    """_summary_
+
+    Args:
+      abstract: Abstract to be stored.
+
+    Returns:
+      id: (objectId): an ObjectId refered to the inserted abstact.
+    """
+    result = db.abstacts.insert_one(abstact.dict())
+    return {"id": str(result.inserted_id)}
+
+@router.get("/{abstact_id}")
+async def read_abstact(abstact_id: str):
+    """_summary_
+
+    Args:
+      abstact ObjectId (str): Abstract id to serach for.
+
+    Returns:
+      Abstact (abstact): Abstract object .
+    """
+    result = db.abstacts.find_one({"_id": ObjectId(abstact_id)})
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail="abstact not found")
---
+++ app/routers/hyperparameter.py
@@ -0,0 +1,37 @@
+"""Router for hyperparameter"""
+
+from bson import ObjectId
+from fastapi import APIRouter, HTTPException
+from ..database import db
+from ..models import Hyperparameter
+
+router = APIRouter(prefix="/Hyperparameter")
+
+@router.post("/hyperparameter")
+async def create_hyperparameter(hyperparameter: Hyperparameter):
+    """_summary_
+
+    Args:
+      hyperparameter: hyperparameter to be stored.
+
+    Returns:
+      id: (objectId): an ObjectId refered to the inserted hyperparameter.
+    """
+    result = db.hyperparameters.insert_one(hyperparameter.dict())
+    return {"id": str(result.inserted_id)}
+
+@router.get("/{hyperparameter_id}")
+async def read_hyperparameter(hyperparameter_id: str):
+    """_summary_
+
+    Args:
+      hyperparameter ObjectId (str): hyperparameter id to serach for.
+
+    Returns:
+      hyperparameter (Hyperparameter): Abstract object .
+    """
+    result = db.hyperparameters.find_one({"_id": ObjectId(hyperparameter_id)})
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail="hyperparameter not found")
