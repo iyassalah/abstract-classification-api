1db9dd7d7d7c4263074059394d80aea3e905a380
iyassalah <iyas.a.salah@transcend.ps>
2023-05-01 19:23:55 +0300

    small change to be able to send request from the same machine.

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from .routers import batch, interactive
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv('../.env.dev')
 
@@ -10,6 +11,14 @@ app = FastAPI()
 app.include_router(batch.router)
 app.include_router(interactive.router)
 
+#just to be able to send the request in the same machine
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
