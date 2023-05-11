d0758be6e74b92cb019c2836b0798794f6e3e5c5
yamen <yamentaad@gmail.com>
2023-05-11 13:37:54 +0300

    Add unique index to email

--- app/database.py
+++ app/database.py
@@ -15,3 +15,5 @@ def setup_db_indexes() -> None:
     index_info = users_col.index_information()
     if "username_1" not in index_info:
         users_col.create_index([("username", 1)], unique=True)
+    if "email_1" not in index_info:
+        users_col.create_index([("email", 1)], unique=True)
