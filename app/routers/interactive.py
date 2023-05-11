871636493b208881c50bd7d35fa2350b5a291ac9
yamen <yamentaad@gmail.com>
2023-05-11 16:16:46 +0300

    #12 Return labels for a PDF instead of the extracted text

--- app/routers/interactive.py
+++ app/routers/interactive.py
@@ -18,10 +18,10 @@ async def classifiy_one(single_abstract: InteractiveModel):
     """_summary_
 
     Args:
-        abstract (str): Abstract to be classified
+        `abstract` (`str`): Abstract to be classified
 
     Returns:
-        List[str]: List of predicted categories for this abstract.
+        `List[str]`: List of predicted categories for this abstract.
     """
     model = get_model()
     return {"categories": model.predict_one(single_abstract.abstract)}
@@ -31,10 +31,10 @@ async def classifiy_one(single_abstract: InteractiveModel):
 async def classifiy_one_proba(
     single_abstract: InteractiveModel,
 ) -> Probabilities:
-    """_summary_
+    """Given an abstract, returns a list of predicted categories for it and their respective probabilities
 
     Args:
-        abstract (str): Abstract to be classified
+        abstract (`str`): Abstract to be classified
 
     Returns:
         Probabilities
@@ -43,12 +43,24 @@ async def classifiy_one_proba(
     return model.predict_proba_one(single_abstract.abstract)
 
 
-@router.post("/pdf", tags=["parser"])
+@router.post("/pdf", tags=["parser"], response_model=CategoriesModel)
 async def extract_abstract_from_pdf(file: UploadFile):
+    """Extract abstract from a PDF file and returns its predicted categories.
+
+    Args:
+        file (`UploadFile`): An uploaded file object in PDF file format.
+
+    Raises:
+        `HTTPException`: If the file is not a PDF.
+        `HTTPException`: If the abstract cannot be extracted.
+
+    Returns:
+        `CategoriesModel`: The predicted categories of the abstract.
+    """
     if file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="File must be a PDF")
     contents = await file.read()
     abstract = extract_from_pdf(contents)
     if abstract:
-        return {"abstract": abstract}
+        return {"categories": get_model().predict_one(abstract)}
     raise HTTPException(400, "Could not locate abstract")
