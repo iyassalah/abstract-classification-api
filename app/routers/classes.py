6422e1789cd7509653f7237e06b454b342aa6e79
Iyas Salah <iyas.a.salah@gmail.com>
2023-05-13 13:30:03 +0300

    add needed doc strings.

--- app/routers/classes.py
+++ app/routers/classes.py
@@ -14,8 +14,11 @@ router = APIRouter(
 
 classes_collection = db["classes"]
 
-# Store all classes from get_classes() at startup
 def store_classes():
+    """
+    Stores all the classes from `get_classes()` into the database at startup.
+
+    """
     classes = get_classes()
     for internal_label in classes:
         query = {"internal_label": internal_label}
@@ -25,22 +28,36 @@ def store_classes():
             result = classes_collection.insert_one(new_class.dict())
             print(f"Inserted new class with id: {result.inserted_id}")
 
-
-# Update the UIClass for a specific modelClass
-@router.put("/classes/{internal_label}")
 async def update_target_lable(internal_label: str, target_lable: str):
-    query = {"modelClass": modelClass}
+    """
+    Updates the target label for a specific internal label.
+
+    Args:
+        internal_label (str): The internal label to be updated.
+        target_lable (str): The new target label.
+
+    Returns:
+        dict: A dictionary containing a message indicating whether the target label was updated successfully or not.
+
+    """
+    query = {"internal_label": internal_label}
     update = {"$set": {"target_lable": target_lable}}
     result = classes_collection.update_one(query, update)
     if result.modified_count == 1:
-        return {"message": "UIClass updated successfully"}
+        return {"message": "Target label updated successfully"}
     else:
         return {"message": "No documents were modified"}
 
-# Return all tags from the database for the UI
-@router.get("/classes/")
 async def get_target_lable():
+    """
+    Returns a list of all the target labels in the database for the UI.
+
+    Returns:
+        dict: A dictionary containing a list of all the target labels in the database.
+
+    """
     target_lables = []
-    for doc in classes_collection.find({}, {"_id": 0, "UIClass": 1}):
+    for doc in classes_collection.find({}, {"_id": 0, "target_lable": 1}):
         target_lables.append(doc["target_lable"])
     return {"target_lables": target_lables}
+
